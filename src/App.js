48c5d17542cb80b2488932a3aeb9ae11d2d2135a
Bhavesh Lokre <55055550+bhavesh1235@users.noreply.github.com>
2021-07-12 23:53:06 +0530

    localStorage Functionality added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect, useDebugValue } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) return JSON.parse(localStorage.getItem("list"));
+  else return [];
+};
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
@@ -24,10 +29,10 @@ function App() {
           return item;
         })
       );
-      setName('')
-      setEditID(null)
-      setIsEditing(false)
-      showAlert(true,'success','value changed')
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "value changed");
     } else {
       //show alert
       showAlert(true, "success", "item added to List");
@@ -57,6 +62,11 @@ function App() {
     setIsEditing(true);
     setName(specificList.title);
   };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
